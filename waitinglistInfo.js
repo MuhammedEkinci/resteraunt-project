65b8be431b34ebab891e50fad6c224fe005ea89e
Adam Jones <adam.w.jones2@gmail.com>
2020-09-08 20:13:34 -0400

    waitingList

---
+++ waitinglistInfo.js
@@ -0,0 +1,10 @@
+const waitingListArray = [
+    {
+        name: "Adam",
+        phoneNumber: "123-456-7890",
+        email: "Adam@email.com",
+        uniqueID: "321"
+    }
+    ];
+    
+    module.exports = waitingListArray;
